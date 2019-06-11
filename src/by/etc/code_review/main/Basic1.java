af6619312d1cf9316871cebc9a5e9aad060a848b
Spawn <alkspawn@gmail.com>
2019-06-11 09:47:57 +0300

    final corr

--- src/by/etc/code_review/main/Basic1.java
+++ src/by/etc/code_review/main/Basic1.java
@@ -134,6 +134,7 @@ public class Basic1 {
 
         //loop
         //1.Напишите  программу,  где  пользователь  вводит любое  целое  положительное  число. А  программа  суммирует все числа от 1 до введенного пользователем числа.
+        System.out.println("Task 1 loop: ");
         Scanner scanner = new Scanner(System.in);
         boolean valid = true;
         int positiveNumber = 0;
